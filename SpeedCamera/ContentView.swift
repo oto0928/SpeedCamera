e427505325c03e1d005dbcbccd8f3c0f166731cd
toa takeuchi <toa0928koron@icloud.com>
2025-09-19 12:52:11 +0900

    Initial Commit

---
+++ SpeedCamera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeedCamera
+//
+//  Created by 竹内音碧 on 2025/09/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeedCamera/SpeedCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedCameraApp.swift
+//  SpeedCamera
+//
+//  Created by 竹内音碧 on 2025/09/19.
+//
+
+import SwiftUI
+
+@main
+struct SpeedCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
